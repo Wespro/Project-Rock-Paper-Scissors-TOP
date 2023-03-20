db0d0a18f829c152ec19bc54762465e17ef5fd70
Abdelrahman <Abdelrahmanmohamed5533@yahoo.com>
2023-03-20 03:40:16 +0200

    Create function called game   Calls the playRound function inside of this one to play a 5 round game   that keeps score and reports a winner or loser at the end.

--- function.js
+++ function.js
@@ -43,4 +43,12 @@ function PlayRound(computerSelection) {
   }
 }
 
-PlayRound(getComputerChoice());
+//function called game().
+//Call the playRound function inside of this one to play a 5 round game
+// that keeps score and reports a winner or loser at the end.
+function game() {
+  for (let i = 0; i < 5; i++) {
+    PlayRound(getComputerChoice());
+  }
+}
+game();
