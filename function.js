f542981919dde8d41c1e99e3b0cd0328cf5543ee
Abdelrahman <Abdelrahmanmohamed5533@yahoo.com>
2023-03-20 03:01:45 +0200

    Create getComputerChoice function That will randomly return either Rock, Paper or Scissors.

--- function.js
+++ function.js
@@ -1,1 +1,12 @@
-console.log("hi");
+function getComputerChoice() {
+  let num = Math.floor(Math.random() * 3 + 1);
+  if (num === 1) {
+    console.log("sis");
+  } else if (num === 2) {
+    console.log("rock");
+  } else {
+    console.log("paper");
+  }
+}
+
+getComputerChoice();
