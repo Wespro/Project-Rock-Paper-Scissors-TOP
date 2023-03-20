47dfeb982d0ea6e356cdaff7bbbe274251d191dd
Abdelrahman <Abdelrahmanmohamed5533@yahoo.com>
2023-03-20 03:29:47 +0200

    Create a function that plays a single round  single round of Rock Paper Scissors.

--- function.js
+++ function.js
@@ -1,12 +1,46 @@
+//  Create getComputerChoice function
+//That will randomly return either Rock, Paper or Scissors.
 function getComputerChoice() {
   let num = Math.floor(Math.random() * 3 + 1);
   if (num === 1) {
-    console.log("sis");
+    return "sissores";
   } else if (num === 2) {
-    console.log("rock");
+    return "rock";
   } else {
-    console.log("paper");
+    return "paper";
   }
 }
 
-getComputerChoice();
+//function that plays a single round of Rock Paper Scissors.
+function PlayRound(computerSelection) {
+  let input = prompt("Pick a rock or paper or sissores");
+  if (input.toLowerCase() === computerSelection) {
+    console.log("it's a tie");
+  } else if (input.toLowerCase() === "rock" && computerSelection === "paper") {
+    console.log("you lose you picked " + input.toLowerCase());
+  } else if (input.toLowerCase() === "paper" && computerSelection === "rock") {
+    console.log("you win you picked " + input.toLowerCase());
+  } else if (
+    input.toLowerCase() === "paper" &&
+    computerSelection === "sissores"
+  ) {
+    console.log("you lose you picked " + input.toLowerCase());
+  } else if (
+    input.toLowerCase() === "sissores" &&
+    computerSelection === "paper"
+  ) {
+    console.log("you win you picked " + input.toLowerCase());
+  } else if (
+    input.toLowerCase() === "sissores" &&
+    computerSelection === "rock"
+  ) {
+    console.log("you lose you picked " + input.toLowerCase());
+  } else if (
+    input.toLowerCase() === "rock" &&
+    computerSelection === "sissores"
+  ) {
+    console.log("you win you picked " + input.toLowerCase());
+  }
+}
+
+PlayRound(getComputerChoice());
